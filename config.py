e040b908bbcffacc2b1d17240fd573ea3ee3ec98
VallyWonka <55382104+VallyWonka@users.noreply.github.com>
2022-05-16 01:54:32 +0300

    Reconfigure

--- config.py
+++ config.py
@@ -1,15 +1,11 @@
 import logging
 import os
-from dotenv import load_dotenv
 
 
-load_dotenv()
-
-
-ADMIN_ID = os.getenv("ADMIN_ID")
-BOT_TOKEN = os.getenv("BOT_TOKEN")
-KITTEN_SOURCE = os.getenv("KITTEN_SOURCE")
-DATABASE_URL = os.getenv("POSTGRES_URL").replace("postgres://", "postgresql://")
+ADMIN_ID = os.environ["ADMIN_ID"]
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+KITTEN_SOURCE = os.environ["KITTEN_SOURCE"]
+DATABASE_URL = os.environ["DATABASE_URL"].replace("postgres://", "postgresql://")
 
 logging.basicConfig(
     format="%(asctime)s - %(name)s - %(levelname)s - %(message)s",
